0ad2e1b438fcbe2d6905871efa1e476323dd04e7
irumvanselme <andesanselme@gmail.com>
2021-07-26 12:18:58 +0200

    Made create my todo

--- utils/db/connection.py
+++ utils/db/connection.py
@@ -4,8 +4,3 @@ from mysql.connector import connect
 def get_connection():
     connection = connect(host='localhost', database='task_pad', user='root', password='Rca@{123}')
     return connection
-
-
-def get_cursor ():
-    connection = get_connection()
-    return connection.cursor()
--- utils/functions.py
+++ utils/functions.py
@@ -1,10 +1,32 @@
+from models.Todo import Todo
+from utils.db.connection import get_connection
+
+
 def print_help():
     print("I am the help")
 
 
 def new_todo(name):
-    print("Creating todo with title ", name)
+    todo = Todo(title=name)
+
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    sql = "INSERT INTO todos VALUES(NULL, %s, NOW(), %s)"
+    val = (todo.title, todo.status.name)
+    cursor.execute(sql, val)
+
+    connection.commit()
+
+    log("Added your todo into the store ... ")
 
 
 def all_todos():
     print("Showing all todos ")
+
+
+def log(message, success=True):
+    if success:
+        print("[SUCCESS]:", message)
+    else:
+        print("[FAILURE]:", message)
