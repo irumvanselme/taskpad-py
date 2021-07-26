01625815ecf14e4c02d73bcf4d92727da46133d7
irumvanselme <andesanselme@gmail.com>
2021-07-26 12:06:01 +0200

    Created the basic functions

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import mysql.connector
+import sys
+
+from getopt import getopt, GetoptError
+from utils.functions import new_todo, all_todos, print_help
+
+
+def main(argv):
+    try:
+        opts, args = getopt(argv, "hi:o:", ["new=", "all"])
+
+        if len(opts) == 0:
+            raise GetoptError("")
+
+        for opt, arg in opts:
+            if opt in ('-h', '--help'):
+                raise GetoptError("")
+            elif opt in ('-n', '--new'):
+                new_todo(arg)
+            elif opt in ('-a', '--all'):
+                all_todos()
+            else:
+                raise GetoptError("")
+    except GetoptError:
+        print_help()
+
 
 if __name__ == '__main__':
-    print("Hello world .... ")
+    main(sys.argv[1:])
---
+++ utils/functions.py
@@ -0,0 +1,10 @@
+def print_help():
+    print("I am the help")
+
+
+def new_todo(name):
+    print("Creating todo with title ", name)
+
+
+def all_todos():
+    print("Showing all todos ")
