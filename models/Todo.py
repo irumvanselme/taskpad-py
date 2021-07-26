c4b5ccff852f2ab27a99db9e2ddafe3e80a0dcc3
irumvanselme <andesanselme@gmail.com>
2021-07-26 11:23:29 +0200

    Add todo status

--- models/Todo.py
+++ models/Todo.py
@@ -1,12 +1,10 @@
-from pip._internal.utils.misc import enum
+from utils.enums import TodoStatus
+from datetime import datetime
 
-status = enum(ACTIBE)
 
 class Todo:
-    id = 0
-    title = ""
-    status = "ACTIVE"
-
-
-    def __init__(self, title):
+    def __init__(self, title, _id=0, status=TodoStatus.ACTIVE, created_at=datetime.now()):
+        self.id = _id
         self.title = title
+        self.status = status
+        self.created_at = created_at
---
+++ utils/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class TodoStatus(Enum):
+    ACTIVE = 1
+    COMPLETED = 2
